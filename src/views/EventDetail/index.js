350c463534a83b368a54bb88b9be0c735eb992a7
Thomas Maximini <tmaximini@gmail.com>
2017-01-06 19:47:19 +0100

    event detail props

--- src/views/EventDetail/index.js
+++ src/views/EventDetail/index.js
@@ -1,15 +1,32 @@
 import React, { Component, PropTypes } from 'react';
 import { View, ListView, StyleSheet, Text } from 'react-native';
-
+import { formatDate } from '../../utils';
 import styles from './styles';
 
 
 export default class Event extends Component {
 
+  static propTypes = {
+    event: PropTypes.object.isRequired,
+  }
+
   render() {
+    const {
+      name,
+      type,
+      description,
+      isPublic,
+      startAt,
+    } = this.props.event;
+
     return (
       <View>
         <Text>EVENT!!!</Text>
+        <Text>{name}</Text>
+        <Text>{type}</Text>
+        <Text>{description}</Text>
+        <Text>{isPublic}</Text>
+        <Text>{formatDate(startAt)}</Text>
       </View>
     );
   }
