92f39fc4c45b98aa324ee4b40346e544738d2cbe
Thomas Maximini <tmaximini@gmail.com>
2017-01-15 23:36:58 +0100

    make EventList stateless

--- src/components/EventList/EventListItem/index.js
+++ src/components/EventList/EventListItem/index.js
@@ -1,6 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Image, View, Text, TouchableHighlight } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
 import { Actions } from 'react-native-router-flux';
 import Date from '../../Date';
 import Location from '../../Location';
@@ -8,62 +7,57 @@ import styles from './styles';
 
 const background = require('../../../assets/sunflowers.jpg');
 
-export default class EventListItem extends Component {
+const EventListItem = ({ event }) => {
+  const { name, startAt, location } = event;
 
-  static propTypes = {
-    event: PropTypes.object.isRequired,
-  }
-
-  render() {
-    const { name, type, startAt, location } = this.props.event;
-
-    const { locality, formattedAddress } = location;
-
-    return (
-      <View
-        style={styles.container}
+  return (
+    <View
+      style={styles.container}
+      source={background}
+    >
+      <Image
         source={background}
+        style={styles.bgImage}
       >
-        <Image
-          source={background}
-          style={styles.bgImage}
-        >
-          <View style={styles.wrapper}>
-            <View style={styles.top}>
+        <View style={styles.wrapper}>
+          <View style={styles.top}>
 
-            </View>
-
-
-            <TouchableHighlight
-              style={styles.middle}
-              onPress={() => Actions.event({ title: name, event: this.props.event })}
-            >
-              <View
-                style={styles.main}
-              >
-                <View style={styles.headline}>
-                  <Text style={styles.title}>
-                    {name}
-                  </Text>
-                  <Text style={styles.button}>
-                    >
-                  </Text>
-                </View>
-              </View>
-            </TouchableHighlight>
-            <View style={styles.bottom}>
-              <Date
-                date={startAt}
-              />
-              <Location
-                location={location}
-              />
-            </View>
           </View>
-        </Image>
-      </View>
-    );
-  }
 
-}
 
+          <TouchableHighlight
+            style={styles.middle}
+            onPress={() => Actions.event({ title: name, event: this.props.event })}
+          >
+            <View
+              style={styles.main}
+            >
+              <View style={styles.headline}>
+                <Text style={styles.title}>
+                  {name}
+                </Text>
+                <Text style={styles.button}>
+                  >
+                </Text>
+              </View>
+            </View>
+          </TouchableHighlight>
+          <View style={styles.bottom}>
+            <Date
+              date={startAt}
+            />
+            <Location
+              location={location}
+            />
+          </View>
+        </View>
+      </Image>
+    </View>
+  );
+};
+
+EventListItem.propTypes = {
+  event: PropTypes.object,
+};
+
+export default EventListItem;
