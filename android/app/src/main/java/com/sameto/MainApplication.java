242e759738c52a804110387bae94d54b6d3d4639
Thomas Maximini <tmaximini@gmail.com>
2017-02-05 22:54:51 +0100

    fix android build, set up fbsdk for android

--- android/app/src/main/java/com/sameto/MainApplication.java
+++ android/app/src/main/java/com/sameto/MainApplication.java
@@ -16,11 +16,19 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -37,7 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
             new VectorIconsPackage(),
             new ReactNativeI18n(),
             new RNGeocoderPackage(),
-            new FBSDKPackage(),
+            new FBSDKPackage(mCallbackManager),
             new BlurViewPackage()
       );
     }
@@ -51,6 +59,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
