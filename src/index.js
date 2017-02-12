54e77b0856080176823bef7426e393c160644c59
Thomas Maximini <tmaximini@gmail.com>
2017-02-12 17:26:30 +0100

    moment locale

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
+import DeviceInfo from 'react-native-device-info';
+import moment from 'moment';
+import 'moment/locale/de';
 import configureStore from './redux/configureStore';
-
 import Routes from './Routes';
 
+moment.locale(DeviceInfo.getDeviceLocale());
 const store = configureStore();
 
 const sameto = () => (
