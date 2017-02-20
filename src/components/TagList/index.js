2b7064e9538a8161ee9c02e5293286613c09a7a5
Thomas Maximini <tmaximini@gmail.com>
2017-02-20 10:16:59 +0100

    translate tags

--- src/components/TagList/index.js
+++ src/components/TagList/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
+import I18n from 'react-native-i18n';
 import Tag from './Tag';
 import styles from './styles';
 
 const TagList = ({ tags, align }) => (
   <View style={[styles.container, { alignItems: align || 'center' }]}>
-    {tags.map((tag, index) => <Tag key={index} tag={tag} />)}
+    {tags.map((tag, index) => <Tag key={index} tag={I18n.t(tag)} />)}
   </View>
 );
 
