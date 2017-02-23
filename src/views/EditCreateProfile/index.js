c32b0f97be9532b77795125ea6ef98565eac86d0
Thomas Maximini <tmaximini@gmail.com>
2017-02-23 11:23:00 +0100

    fix validation bug

--- src/views/EditCreateProfile/index.js
+++ src/views/EditCreateProfile/index.js
@@ -64,7 +64,7 @@ export default class EditCreateProfile extends Component {
     // required fields
     const { firstName, lastName, location, interests } = profile;
 
-    return firstName && lastName && location && interests.length;
+    return firstName && lastName && location && interests && interests.length;
   }
 
   handleImageUpload() {
