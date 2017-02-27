bf22855e7922cfe63e56cd8a3921a29acd60dde0
Thomas Maximini <tmaximini@gmail.com>
2017-02-27 12:40:12 +0100

    fix PN android

--- src/hocs/FCM.js
+++ src/hocs/FCM.js
@@ -17,21 +17,26 @@ export default function (WrappedComponent) {
     }
 
     componentDidMount() {
+      console.log('mouting fcm');
       FCM.requestPermissions(); // for iOS
+
       FCM.getFCMToken().then(token => {
         console.log({ token });
         // store fcm token in your server
         updateFcmToken(token);
       });
+
       this.notificationListener = FCM.on(FCMEvent.Notification, async (notif) => {
+        console.log('notif', notif);
         // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
         if (notif.local_notification) {
           //this is a local notification
+          return;
         }
         if (notif.opened_from_tray) {
           //app is open/resumed because user clicked banner
+          return;
         }
-        // await someAsyncCall();
 
         if (Platform.OS === 'ios') {
           //optional
@@ -51,24 +56,57 @@ export default function (WrappedComponent) {
             default:
               break;
           }
-          this.showLocalNotification(notif);
         }
+        this.showLocalNotification(notif);
       });
+
       this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, (token) => {
         console.log('refresh token', token);
         // fcm token may not be available on first load, catch it here
         updateFcmToken(token);
       });
+
     }
 
+
+    // FCM.presentLocalNotification({
+    //   id: 'UNIQ_ID_STRING',                               // (optional for instant notification)
+    //   title: 'My Notification Title',                     // as FCM payload
+    //   body: 'My Notification Message',                    // as FCM payload (required)
+    //   sound: 'default',                                   // as FCM payload
+    //   priority: 'high',                                   // as FCM payload
+    //   click_action: 'ACTION',                             // as FCM payload
+    //   badge: 10,                                          // as FCM payload IOS only, set 0 to clear badges
+    //   number: 10,                                         // Android only
+    //   ticker: 'My Notification Ticker',                   // Android only
+    //   auto_cancel: true,                                  // Android only (default true)
+    //   large_icon: 'ic_launcher',                           // Android only
+    //   icon: 'ic_launcher',                                // as FCM payload, you can relace this with custom icon you put in mipmap
+    //   big_text: 'Show when notification is expanded',     // Android only
+    //   sub_text: 'This is a subText',                      // Android only
+    //   color: 'red',                                       // Android only
+    //   vibrate: 300,                                       // Android only default: 300, no vibration if you pass null
+    //   tag: 'some_tag',                                    // Android only
+    //   group: 'group',                                     // Android only
+    //   my_custom_data: 'my_custom_field_value',             // extra data you want to throw
+    //   lights: true,                                       // Android only, LED blinking (default false)
+    //   show_in_foreground: true                            // notification when app is in foreground (local & remote)
+    // })
+
     showLocalNotification(notif) {
+      console.log('local notif', notif);
       FCM.presentLocalNotification({
-        title: notif.title,
-        body: notif.body,
+        title: notif.fcm.title,
+        body: notif.fcm.body,
+        sound: 'default',
+        badge: 1,
+        number: 1,
+        icon: 'icon',
+        large_icon: 'ic_launcher',
         priority: 'high',
         click_action: notif.click_action,
         show_in_foreground: true,
-        local: true
+        // local: true
       });
     }
 
